d2f7f76113f7e774789c9c32e85a7fa451d2c501
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-05-26 01:42:48 +0800

    add image

--- FLASK-ANGULAR-APP/angularposts/src/app/flaskapi.service.ts
+++ FLASK-ANGULAR-APP/angularposts/src/app/flaskapi.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http"
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { Post } from './models/Post'
+import { Post } from './models/Post';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,22 +14,48 @@ export class FlaskapiService {
     private httpClient: HttpClient
   ) { }
 
-  public server: string = 'http://localhost:5000/api/';
-  public getPosts() {
-    return this.httpClient.get<Post>(this.server + 'posts');
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  public server = 'http://localhost:5000/api/';
+  public getPosts(): Observable<Post[]> {
+    return this.httpClient.get<Post[]>(this.server + 'posts')
+    .pipe(
+      catchError(this.handleError<Post[]>('getPosts', []))
+    );
   }
   public getPost(postId: string) {
-    return this.httpClient.get<Post>(this.server + `post/${postId}`)
+    return this.httpClient.get<Post>(this.server + `post/${postId}`);
   }
   public addPost(postObj: Post, image: any) {
     const {title, content} = postObj;
     const formData: FormData = new FormData();
 
-    formData.append("title", title);
-    formData.append("content", content);
-    formData.append("cover", image[0], image['filename']);
+    formData.append('title', title);
+    formData.append('content', content);
+    formData.append('cover', image[0], image.filename);
 
-    return this.httpClient.post<Post>(this.server + "addpost", formData);
+    return this.httpClient.post<Post>(this.server + 'addpost', formData);
+  }
+
+  public getImage(imageName: string): string {
+    // get Image from backend
+    return this.server + `image/${imageName}`;
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      // this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
 }
